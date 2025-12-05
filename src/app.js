ee9092cee277a8dafd78ebe0502e71957292aace
vanshgoel123 <vanshg247776@gmail.com>
2025-12-06 03:48:03 +0530

    public error

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(cors({
 
 app.use(express.json({limit:'20mb'}));
 app.use(express.urlencoded({extended:true, limit:'20mb'}));
-app.use(express.static("public"))
+app.use("/public", express.static("public"));
 app.use(cookie()) //global middleware hai ye. har req mei cookie as header bhejta hai
 
 
