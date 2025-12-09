7cf7f030381454491987bfda24e83bacf9c0760f
vanshgoel123 <vanshg247776@gmail.com>
2025-12-09 23:49:59 +0530

    cors origin to fixed

--- src/app.js
+++ src/app.js
@@ -7,10 +7,12 @@ console.log("cors---->",process.env.CORS_ORIGIN)
 
 const app = express();
 app.use(cors({
-  origin:process.env.CORS_ORIGIN,
-  credentials:true
+  origin: "https://172.16.61.173",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
-}))
 
 app.use(express.json({limit:'16kb'}))
 app.use(express.urlencoded({extended:true,limit:'16kb'}))
