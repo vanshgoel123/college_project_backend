468990135b865945457dc2793ee1dabf40e4b3f5
vanshgoel123 <vanshg247776@gmail.com>
2025-12-13 22:42:58 +0530

    test added

--- tests/health.test.mjs
+++ tests/health.test.mjs
@@ -1,5 +1,6 @@
-const request =  require("supertest");
-const app =require("../src/app")
+import request from "supertest";
+import { app } from "../src/app.js";
+
 describe("Backend basic test",()=>{
     test("Health check should return 200",async()=>{
         const res= await request(app).get("/health");
